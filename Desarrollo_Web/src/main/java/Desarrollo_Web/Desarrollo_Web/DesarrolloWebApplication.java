02db64ce4e8aad8b1ea57cceb056bd4663f70753
Roberto2401 <robertoiribarren020@gmail.com>
2023-01-17 20:11:05 -0600

    primer Commit de inicializacion

---
+++ Desarrollo_Web/src/main/java/Desarrollo_Web/Desarrollo_Web/DesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package Desarrollo_Web.Desarrollo_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloWebApplication.class, args);
+	}
+
+}
---
+++ Desarrollo_Web/src/test/java/Desarrollo_Web/Desarrollo_Web/DesarrolloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package Desarrollo_Web.Desarrollo_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
