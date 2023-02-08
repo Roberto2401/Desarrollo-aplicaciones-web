b72898657d3a7830a2f4c8fc0a774bc2a3c8977b
Roberto2401 <robertoiribarren020@gmail.com>
2023-02-07 21:06:13 -0600

    commit semana 4
    se inicia el backend

---
+++ Desarrollo_Web/src/main/java/Desarrollo_Web/Desarrollo_Web/Entity/pais.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Desarrollo_Web.Desarrollo_Web.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+
+/**
+ *
+ * @author Roberto
+ */
+@Entity 
+@Table (name="paises")
+public class pais {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private long id;
+    private String pais;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    
+    }
